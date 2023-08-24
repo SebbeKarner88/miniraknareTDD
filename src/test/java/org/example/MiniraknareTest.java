11fea4d288762c48783a9d9b3a11a5b27ec16379
Sebastian Kärner <sebastiankarner@live.se>
2023-08-24 12:58:08 +0200

    some more tests

--- src/test/java/org/example/MiniraknareTest.java
+++ src/test/java/org/example/MiniraknareTest.java
@@ -9,12 +9,54 @@ class MiniraknareTest {
     @Test
     public void addition() {
         //Given
-        int a = 2;
-        int b = 4;
+        int term1 = 2;
+        int term2 = 4;
         //When
-        int result = Miniraknare.addition(a, b);
+        int result = Miniraknare.addition(term1, term2);
         //Then
         assertEquals(result, 6);
     }
 
+    @Test
+    public void subtraction() {
+        //Given
+        int term1 = 4;
+        int term2 = 2;
+        //When
+        int result = Miniraknare.subtraction(term1, term2);
+        //Then
+        assertEquals(result, 2);
+    }
+
+    @Test
+    public void multiplication() {
+        //Given
+        int factor1 = 2;
+        int factor2 = 4;
+        //When
+        int result = Miniraknare.multiplication(factor1, factor2);
+        //Then
+        assertEquals(result, 8);
+    }
+
+    @Test
+    public void division() {
+        //Given
+        int numerator = 4;
+        int denominator = 2;
+        //When
+        int result = Miniraknare.division(numerator, denominator);
+        //Then
+        assertEquals(result, 2);
+    }
+
+    @Test
+    public void squaredPositive() {
+        //Given
+        int base = 4;
+        //When
+        int result = Miniraknare.squared(base);
+        //Then
+        assertEquals(result, 16);
+    }
 }
